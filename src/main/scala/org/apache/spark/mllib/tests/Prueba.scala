ec7db15353bb49e5fd785675c2a10e72b2cfa55f
eirasf <carlos.eiras.franco@udc.es>
2015-05-18 13:15:41 +0200

    Creado proyecto

---
+++ src/main/scala/org/apache/spark/mllib/tests/Prueba.scala
@@ -0,0 +1,33 @@
+package org.apache.spark.mllib.tests
+
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.feature.InfoThCriterionFactory
+import org.apache.spark.mllib.feature.InfoThSelector
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.SparkConf
+
+object HelloWorld {
+    def main(args: Array[String]) {
+      val conf = new SparkConf().setAppName("Prueba").setMaster("local")
+      val sc=new SparkContext(conf)
+      val criterion = new InfoThCriterionFactory("mrmr")
+      val nToSelect = 100
+      val nPool = 100 // 0 -> w/o pool
+      val data: RDD[LabeledPoint] = MLUtils.loadLibSVMFile(sc, "/home/eirasf/Escritorio/covtype.scale01")
+      
+      println("*** FS criterion: " + criterion.getCriterion.toString)
+      println("*** Number of features to select: " + nToSelect)
+      println("*** Pool size: " + nPool)
+      
+      val featureSelector = InfoThSelector.train(criterion, 
+              data, // RDD[LabeledPoint]
+              nToSelect, // number of features to select
+            nPool) // number of features in pool
+          featureSelector
+      
+      val reduced = data.map(i => LabeledPoint(i.label, featureSelector.transform(i.features)))
+      reduced.first()
+    }
+  }
